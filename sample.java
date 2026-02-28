8759ffd8c77cefc64cd115b187920887582c049d
980714 <rahul.charala@gmail.com>
2026-02-28 19:48:27 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,13 +2,7 @@ import java.util.Scanner;
 
 public class sample {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
-        System.out.println("Hello " + name + "!");
-        System.out.println("wel-come to java program");
         System.out.println("wel-come to java program");
 
 
