fbdd3e7063235de45b90869b1a1c74463af667cf
980714 <rahul.charala@gmail.com>
2026-02-28 20:42:49 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -4,6 +4,7 @@ public class sample {
     public static void main(String[] args) {
 
         System.out.println("wel-come to java program");
+        System.out.println("wel-come to web-hook");
 
     }
 }
